8c4130f3ee95cd1a733b5876329b375ce57c839f
Camille Jakob <camillejakob@outlook.com>
2025-09-10 15:37:46 +0100

    Fix test fake calls
    Fix the test fake calls as previously it was still hitting the actual API.

--- tests/Feature/LookupTest.php
+++ tests/Feature/LookupTest.php
@@ -10,128 +10,133 @@ class LookupTest extends TestCase
     /** @test */
     public function lookup_minecraft_profile_by_username()
     {
-        Http::fake([
-            'https://api.mojang.com/users/profiles/minecraft/*' => Http::response([
-                'id' => '069a79f444e94726a5befca90e38aaf5',
-                'username' => 'Notch',
-                'avatar' => 'https://crafatar.com/avatars069a79f444e94726a5befca90e38aaf5',
-            ], 200),
-        ]);
+        Http::fake(
+            Http::response([
+                'id' => '123',
+                'username' => 'johnd',
+            ])
+        );
 
-        $response = $this->getJson('/lookup?type=minecraft&username=Notch');
+        $response = $this->getJson('/lookup?type=minecraft&username=johnd');
 
         $response->assertOk()
-            ->assertJson([
-                'username' => 'Notch',
-                'id'       => '069a79f444e94726a5befca90e38aaf5',
-                'avatar'   => 'https://crafatar.com/avatars069a79f444e94726a5befca90e38aaf5',
-            ]);
+            ->assertJson(['data' =>[
+                'id'       => '123',
+                'username' => 'johnd',
+                'avatar'   => 'https://crafatar.com/avatars123',
+            ]]);
     }
 
     /** @test */
     public function lookup_minecraft_profile_by_uuid()
     {
-        Http::fake([
-            'https://sessionserver.mojang.com/session/minecraft/profile/*' => Http::response([
-                'id' => '069a79f444e94726a5befca90e38aaf5',
-                'username' => 'Notch',
-                'avatar'   => 'https://crafatar.com/avatars069a79f444e94726a5befca90e38aaf5',
+        Http::fake(
+            Http::response([
+                'id' => '123',
+                'username' => 'johnd',
             ])
-        ]);
+        );
 
-        $this->getJson('/lookup?type=minecraft&id=069a79f444e94726a5befca90e38aaf5')
+        $this->getJson('/lookup?type=minecraft&id=123')
             ->assertOk()
-            ->assertJson([
-                'id' => '069a79f444e94726a5befca90e38aaf5',
-                'username' => 'Notch',
-                'avatar'   => 'https://crafatar.com/avatars069a79f444e94726a5befca90e38aaf5',
-            ]);
+            ->assertJson(['data' => [
+                'id' => '123',
+                'username' => 'johnd',
+            ]]);
     }
 
     /** @test */
     public function lookup_steam_profile_by_username()
     {
-        $this->markTestSkipped();
-        HTTP::fake([
-            'https://ident.tebex.io/usernameservices/4/username/' => Http::response([
-                'id' => '76561198000000000',
-                'username' => 'Notch',
-                'avatar'   => 'https://crafatar.com/avatars069a79f444e94726a5befca90e38aaf5',
-            ])
-        ]);
-
         $this->getJson('/lookup?type=steam&username=Notch')
             ->assertInvalid([
                 'username' => 'Steam only supports IDs'
             ]);
+
+        Http::assertNothingSent();
     }
 
     /** @test */
     public function lookup_steam_profile_by_uuid()
     {
-        HTTP::fake([
-            'https://ident.tebex.io/usernameservices/4/username/*' => Http::response([
-                'id' => '76561198000000000',
-                'username' => 'jolyne cujoh',
-                'avatar'   => 'https://avatars.steamstatic.com/9b07ffdad4718b9b131aa2df88478a8ff45181eb.jpg',
+        Http::fake(
+            Http::response([
+                'id' => '123',
+                'username' => 'johnd',
+                'meta' => [
+                    'avatar' => 'https://avatars.steamstatic.com/123.jpg',
+                ],
             ])
-        ]);
+        );
 
-        $this->getJson('/lookup?type=steam&id=76561198000000000')
+        $this->getJson('/lookup?type=steam&id=123')
             ->assertOk()
             ->assertJson([
-                'id' => '76561198000000000',
-                'username' => 'jolyne cujoh',
-                'avatar'   => 'https://avatars.steamstatic.com/9b07ffdad4718b9b131aa2df88478a8ff45181eb.jpg',
-            ]);
+                    'data' => [
+                        'id' => '123',
+                        'username' => 'johnd',
+                        'avatar'   => 'https://avatars.steamstatic.com/123.jpg',
+                    ]
+                ]
+            );
     }
 
     /** @test */
     public function lookup_xbl_profile_by_uuid()
     {
-        HTTP::fake([
-            'https://ident.tebex.io/usernameservices/3/username/' => Http::response([
-                'id' => '2535453759792258',
-                'username' => 'Notch',
-                'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/2535453759792258/avatarpic-l.png',
+        Http::fake(
+            Http::response([
+                'id' => '123',
+                'username' => 'johnd',
+                'meta' => [
+                    'avatar' => 'https://avatar-ssl.xboxlive.com/avatar/123/avatarpic-l.png',
+                ],
             ])
-        ]);
+        );
 
-        $this->getJson('/lookup?type=xbl&id=2535453759792258')
+        $this->getJson('/lookup?type=xbl&id=123')
             ->assertOk()
             ->assertJson([
-                'id' => '2535453759792258',
-                'username' => 'Notch',
-                'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/2535453759792258/avatarpic-l.png',
-            ]);
+                'data' =>
+                    [
+                        'id' => '123',
+                        'username' => 'johnd',
+                        'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/123/avatarpic-l.png',
+                    ]
+                ]);
     }
 
     /** @test */
     public function lookup_xbl_profile_by_username()
     {
-        HTTP::fake([
-            'https://xbl.io/api/v2/profile/xuid/' => Http::response([
-                'id' => '2535453759792258',
-                'username' => 'Notch',
-                'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/2535453759792258/avatarpic-l.png',
+        HTTP::fake(
+            Http::response([
+                'id' => '123',
+                'username' => 'johnd',
+                'meta' => [
+                    'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/123/avatarpic-l.png',
+                ],
             ])
-        ]);
+        );
 
-        $this->getJson('/lookup?type=xbl&username=Notch')
+        $this->getJson('/lookup?type=xbl&username=johnd')
             ->assertOk()
             ->assertJson([
-                'id' => '2535453759792258',
-                'username' => 'Notch',
-                'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/2535453759792258/avatarpic-l.png',
+                'data' => [
+                    'id' => '123',
+                    'username' => 'johnd',
+                    'avatar'   => 'https://avatar-ssl.xboxlive.com/avatar/123/avatarpic-l.png',
+                ]
             ]);
+
     }
 
     /** @test */
     public function lookup_unsupported_type()
     {
-        $this->getJson('/lookup?type=psn&username=Notch')
+        $this->getJson('/lookup?type=psn&username=johnd')
             ->assertInvalid([
-                'type' => 'Unsupported type'
+                'type' => 'The selected type is invalid.',
             ]);
     }
 }
